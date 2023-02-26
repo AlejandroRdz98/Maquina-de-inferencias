9a48a0be303a4efa3910bedbaeb154a7a46f9d33
AlejandroRdz98 <117599809+AlejandroRdz98@users.noreply.github.com>
2023-02-26 16:42:01 -0600

    Add files via upload
    Máquina de inferencia de Animales

---
+++ code.js
@@ -0,0 +1,509 @@
+const animals =[
+    {
+        nombre: "foca",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "acuatico",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "aleta",
+        tipoExtremidadI: "aleta",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma: "polar"
+        
+    },
+    {
+        nombre: "pingüino",
+        clase: "vertebrado",
+        subClase: "ave",
+        habitad: "acuatico",
+        dieta: "carnivoro",
+        nacimiento: "oviparo",
+        tipoExtremidadS: "aleta",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma:"tundra"
+    },
+    {
+        nombre: "oso",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma:"tundra"
+    },
+    {
+        nombre: "perro",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "si",
+        bioma:"ciudad"
+    },
+    {
+        nombre: "conejo",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "herbivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "si",
+        bioma:"pradera"
+    },
+    {
+        nombre: "tortuga",
+        clase: "vertebrado",
+        subClase: "reptil",
+        habitad: "acuatico",
+        dieta: "omnivoro",
+        nacimiento: "oviparo",
+        tipoExtremidadS: "aleta",
+        tipoExtremidadI: "aleta",
+        cantidadE: "4",
+        sangre: "fria",
+        domesticable: "si",
+        bioma:"tropical"
+    },
+    {
+        nombre: "gallo",
+        clase: "vertebrado",
+        subClase: "ave",
+        habitad: "terrestre",
+        dieta: "omnivoro",
+        nacimiento: "oviparo",
+        tipoExtremidadS: "ala",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "si",
+        bioma:"matorral"
+    },
+    {
+        nombre: "tiburon",
+        clase: "vertebrado",
+        subClase: "pez",
+        habitad: "acuatico",
+        dieta: "carnivoro",
+        nacimiento: "oviparo",
+        tipoExtremidadS: "aleta",
+        tipoExtremidadI: "aleta",
+        cantidadE: "4",
+        sangre: "fria",
+        domesticable: "no",
+        bioma: "tropical"
+    },
+    {
+        nombre: "ardilla",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "herbivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "si",
+        bioma: "bosque"
+    },
+    {
+        nombre: "mapache",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "si",
+        bioma: "bosque"
+    },
+    {
+        nombre: "leopardo",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma: "bosque"
+    },
+    {
+        nombre: "gato",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "si",
+        bioma: "ciudad"
+    },
+    {
+        nombre: "leon",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma:"sabana"
+    },
+    {
+        nombre: "hormiga",
+        clase: "invertebrado",
+        subClase: "artropodo",
+        habitad: "terrestre",
+        dieta: "herbivoro",
+        nacimiento: "oviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "6",
+        sangre: "fria",
+        domesticable: "si",
+        bioma: "prados"
+    },
+    {
+        nombre: "tigre",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma: "selva"
+    },
+    {
+        nombre: "mamut",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "herbivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma:"estepa",
+    },
+    {
+        nombre: "pantera",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma:"selva"
+    },
+    {
+        nombre: "jirafa",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "herbivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma: "sabana",
+    },
+    {
+        nombre: "mariposa",
+        clase: "invertebrado",
+        subClase: "artropodo",
+        habitad: "aereo",
+        dieta: "herbivoro",
+        nacimiento: "oviparo",
+        tipoExtremidadS: "ala",
+        tipoExtremidadI: "pata",
+        cantidadE: "8",
+        sangre: "fria",
+        domesticable: "no",
+        bioma: "bosque",
+    },
+    {
+        nombre: "mosca",
+        clase: "invertebrado",
+        subClase: "artropodo",
+        habitad: "aereo",
+        dieta: "omnivoro",
+        nacimiento: "oviparo",
+        tipoExtremidadS: "ala",
+        tipoExtremidadI: "pata",
+        cantidadE: "8",
+        sangre: "fria",
+        domesticable: "no",
+        bioma: "ciudad"
+    },
+    {
+        nombre: "medusa",
+        clase: "invertebrado",
+        subClase: "cnidarios",
+        habitad: "acuatico",
+        dieta: "carnivoro",
+        nacimiento: "oviparo",
+        tipoExtremidadS: "tentaculos",
+        tipoExtremidadI: "tentaculos",
+        cantidadE: "more",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma: "tropical"
+    },
+    {
+        nombre: "delfin",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "acuatico",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "aleta",
+        tipoExtremidadI: "aleta",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma: "tropical"
+
+    },
+    {
+        nombre: "puma",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "terrestre",
+        dieta: "carnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "pata",
+        tipoExtremidadI: "pata",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma: "estepa"
+    },
+    {
+        nombre: "vaca marina",
+        clase: "vertebrado",
+        subClase: "mamifero",
+        habitad: "acuatico",
+        dieta: "omnivoro",
+        nacimiento: "viviparo",
+        tipoExtremidadS: "aleta",
+        tipoExtremidadI: "aleta",
+        cantidadE: "4",
+        sangre: "caliente",
+        domesticable: "no",
+        bioma: "calido"
+    }
+]
+
+
+let vertebrado = document.getElementById("vertebrado");
+let invertebrado =  document.getElementById("invertebrado");
+let vertebradosC = document.getElementById ("vertebrados");
+let invertebradosC = document.getElementById("invertebrados")
+let questions = document.getElementById("questions")
+let actuar =  document.getElementById("actuar")
+
+actuar.addEventListener("click", function(e){
+    e.preventDefault()
+    obtenerDatos()
+})
+
+vertebrado.addEventListener("click", function(e){
+    
+   console.log("soy un vertebrado")
+  vertebradosC.removeAttribute("hidden")  
+  invertebradosC.setAttribute("hidden", "true") 
+  questions.removeAttribute("hidden") 
+})
+
+invertebrado.addEventListener("click", function(e){
+    console.log("soy un invertebrado")
+    invertebradosC.removeAttribute("hidden")
+    vertebradosC.setAttribute("hidden", "true")
+    questions.removeAttribute("hidden") 
+})
+
+//Obtener datos del formulario
+
+
+
+function obtenerDatos (){
+let clase = document.getElementsByName("clase")
+let subClase = document.getElementsByName("subclase")
+let desplazar =  document.getElementsByName("desplazar")
+let dieta =  document.getElementsByName("dieta")
+let nacer =  document.getElementsByName("nacer")
+let extremidadesS = document.getElementsByName("extremidadesS")
+let extremidadesI = document.getElementsByName("extremidadesI")
+let total =  document.getElementsByName("total")
+let sangre = document.getElementsByName("sangre")
+let domestico = document.getElementsByName("domestico")
+let bioma = document.getElementsByName("bioma")
+    //clase activa
+
+    for(let clas of clase){
+        if(clas.checked){
+            clase = clas.value
+            
+        }
+    }
+
+    for(let subclas of subClase){
+        if(subclas.checked){
+            subClase = subclas.value
+        }
+    }
+    for(let des of desplazar){
+        if(des.checked){
+            desplazar = des.value
+        }
+    }
+    for(let diet of dieta){
+        if(diet.checked){
+            dieta = diet.value
+        }
+    }
+    for(let born of nacer){
+        if(born.checked){
+            nacer = born.value
+        }
+    }
+    for(let xtremiS of extremidadesS){
+        if(xtremiS.checked){
+            extremidadesS = xtremiS.value
+        }
+    }
+    for(let xtremisI of extremidadesI){
+        if(xtremisI.checked){
+            extremidadesI = xtremisI.value
+        }
+    }
+    for(let tot of total){
+        if(tot.checked){
+            total = tot.value
+        }
+    }
+    for(let blood of sangre){
+        if(blood.checked){
+            sangre = blood.value
+        }
+    }
+    for(let domestic of domestico){
+        if(domestic.checked){
+            domestico = domestic.value
+        }
+    }
+    for(let biom of bioma){
+        if(biom.checked){
+            bioma = biom.value
+        }
+    }
+    
+    calcularAnimal({
+        clase: clase,
+        subClase: subClase,
+        habitad: desplazar,
+        dieta: dieta,
+        nacimiento: nacer,
+        tipoExtremidadS: extremidadesS,
+        tipoExtremidadI: extremidadesI,
+        cantidadE: total,
+        sangre: sangre,
+        domesticable: domestico,
+        bioma: bioma,
+    })
+    
+}
+
+
+
+const calcularAnimal = (animalP) =>{
+    let array = [] 
+
+    
+
+
+
+    animals.map(animal =>{
+       
+        //Validar que sea el mismo
+        if (animal.clase == animalP.clase) {
+            if(animal.subClase == animalP.subClase){
+                if (animal.habitad == animalP.habitad) {
+                    if (animal.dieta == animalP.dieta) {
+                        if (animal.nacimiento == animalP.nacimiento) {
+                            if (animal.tipoExtremidadS == animalP.tipoExtremidadS) {
+                                if (animal.tipoExtremidadI == animalP.tipoExtremidadI) {
+                                    if (animal.cantidadE == animalP.cantidadE) {
+                                        if (animal.sangre == animalP.sangre) {
+                                            if (animal.domesticable == animalP.domesticable) {
+                                                if (animal.bioma == animalP.bioma) {
+                                                    array.push(animal.nombre)
+                                                    
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    
+    })
+
+    if (array.length == 0) {
+        alert("¡Seguimos adivinando!")
+    }else{
+
+    
+    alert(`El animal en el que piensas es... ${array}`)
+    console.log(array)}
+}
+
